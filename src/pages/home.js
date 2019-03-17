a523b2155de8cbd7d7448336e4400e821aadc6ba
Mark Shropshire <mdshrops@shropnet.net>
2019-03-17 12:36:56 -0400

    Setup a home page to pull the stockwatch home page data from Stockwatch API

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,33 @@
 import React from "react"
-const HomePage = () => (
+
+export const query = graphql`
+  query allNodeBasicPage {
+    allNodeBasicPage {
+      edges {
+        node {
+          id
+          title
+          body {
+            value
+            format
+            processed
+            summary
+          }
+        }
+      }
+    }
+  }
+`
+
+const HomePage = ({ data }) => (
   <div>
-    <h1>Stockwatch Home!</h1>
+    <h1>Stockwatch</h1>
+    {data.allNodeBasicPage.edges.map(({ node }) => (
+      <div>
+        <h3>{node.title}</h3>
+        <div dangerouslySetInnerHTML={{ __html: node.body.value }} />
+      </div>
+    ))}
   </div>
 )
 export default HomePage
