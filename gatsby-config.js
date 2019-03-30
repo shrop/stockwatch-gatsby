06e1cbfa870ea7fb0aab26b2c2436203c80c13ac
Mark Shropshire <mdshrops@shropnet.net>
2019-03-30 17:40:13 -0400

    Updated home page and Drupal API

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,12 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-drupal',
+      resolve: "gatsby-source-drupal",
       options: {
-        baseUrl: 'http://stockwatch-api.shropnet.net/',
-        apiBase: 'api', // endpoint of Drupal server
+        baseUrl: "https://stockwatch-api.shropnet.net/",
+        apiBase: "api", // endpoint of Drupal server
       },
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
 
 export const query = graphql`
   query allNodeBasicPage {
